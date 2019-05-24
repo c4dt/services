4e99e655c88743c7785486f3626f180d9ad4cffb
tharvik <tharvik@users.noreply.github.com>
2019-05-24 12:04:56 +0200

    conode also build docker image

--- mk/service/conode.mk
+++ mk/service/conode.mk
@@ -23,6 +23,29 @@ $(foreach p,$($SPROTOS),$(call conode-proto,$p))
 $Sconode-proto: $(foreach p,$($SPROTOS),$(realpath $Dconode/proto/$p/$p.pb.go))
 endif
 
+$Dconode/cothority_template:
+	git clone https://github.com/dedis/cothority_template $@
+	# TODO not very indempotent
+	echo 'replace github.com/c4dt/$(service)/conode => ../' >> $@/go.mod
+$Dconode/cothority_template/conode/main.go: | $Dconode/cothority_template
+	echo package main					> $@
+	echo import _ \"github.com/c4dt/$(service)/conode\"	>> $@
+$Dconode/cothority_template/conode/conode: $Dconode/*.go | $Dconode/cothority_template/conode/main.go
+	cd $(dir $@) && GO111MODULE=on GOOS=linux GOARCH=amd64 go build -o $(notdir $@)
+$Dconode/cothority_template/conode/conode_data/private.toml: $Dconode/cothority_template/conode/conode
+	( echo localhost:7770; echo; echo $(dir $@); echo; echo ) | $< setup
+$Dconode/cothority_template/conode/exe/conode.Linux.x86_64: $Dconode/cothority_template/conode/main.go
+	mkdir -p $(dir $@)
+	cp $^ $@
+.PHONY: $Sconode-docker-build
+$Sconode-docker-build: $Dconode/cothority_template/conode/Dockerfile-dev | $Dconode/cothority_template/conode/exe/conode.Linux.x86_64
+	 docker build --tag c4dt/$(service)-conode:latest --file $< $(dir $<)
+.PHONY: $Sconode-docker-run
+$Sconode-docker-run: | $Sconode-docker-build $Dconode/cothority_template/conode/conode_data/private.toml
+	docker run --rm --publish 7770-7771:7770-7771 \
+		--volume $(CURDIR)/$Dconode/cothority_template/conode/conode_data:/conode_data \
+		c4dt/$(service)-conode:latest
+
 .PHONY: $Sconode-build
 $Sconode-build:
 	cd $Dconode && GO111MODULE=on go build
