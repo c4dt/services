ff692eba963f16490da33d0f2dff86db57ef96a0
tharvik <tharvik@users.noreply.github.com>
2019-05-24 16:03:08 +0200

    conode: correctly gen from protos

--- mk/service/conode.mk
+++ mk/service/conode.mk
@@ -12,16 +12,16 @@ $(GOPATH)/bin/protoc-gen-go:
 
 # $1	proto pkg
 define conode-proto =
-$Dconode/proto/$1/$1.pb.go: private PATH := $$(PATH):$$(GOPATH)/bin
-$Dconode/proto/$1/$1.pb.go: $Dprotobuf/$1.proto | $$(GOPATH)/bin/protoc-gen-go
-	TODO? mkdir -p $(dir $@)
+$Dconode/proto/$1/$1.pb.go: private PATH := $(PATH):$(GOPATH)/bin
+$Dconode/proto/$1/$1.pb.go: $Dprotobuf/$1.proto | $(GOPATH)/bin/protoc-gen-go
+	mkdir -p $$(dir $$@)
 	cd $Dprotobuf && protoc --go_out=../$$(dir $$@) $$(notdir $$^)
 endef
-$(foreach p,$($SPROTOS),$(call conode-proto,$p))
+$(foreach p,$($SPROTOS),$(eval $(call conode-proto,$p)))
 
 # TODO for now, we can only generate for flat protobuf hierarchy
 .PHONY: $Sconode-proto
-$Sconode-proto: $(foreach p,$($SPROTOS),$(realpath $Dconode/proto/$p/$p.pb.go))
+$Sconode-proto: $(foreach p,$($SPROTOS),$Dconode/proto/$p/$p.pb.go)
 endif
 
 $Dconode/cothority_template:
