dca3f7433ccdaa63b1e5d8f7e807731feb2a64c4
tharvik <tharvik@users.noreply.github.com>
2020-08-31 16:10:01 +0200

    protobuf: fixes for mac os x

--- mk/service/protobuf.mk
+++ mk/service/protobuf.mk
@@ -9,15 +9,12 @@ $Dprotobuf/node_modules/protobufjs/.installed:
 $Dprotobuf/node_modules/.bin/pbjs: | $Dprotobuf/node_modules/protobufjs/.installed
 $Dprotobuf/node_modules/.bin/pbts: | $Dprotobuf/node_modules/protobufjs/.installed
 
-$Dprotobuf/proto.json: PATH := $(PATH):$Dprotobuf/node_modules/.bin
 $Dprotobuf/proto.json: | $Dprotobuf/node_modules/.bin/pbjs
-	pbjs -t json -o $@ $^
-$Dprotobuf/proto.js: PATH := $(PATH):$Dprotobuf/node_modules/.bin
+	$Dprotobuf/node_modules/.bin/pbjs -t json -o $@ $^
 $Dprotobuf/proto.js: | $Dprotobuf/node_modules/.bin/pbjs
-	pbjs -t static-module -o $@ $^
-$Dprotobuf/proto.d.ts: PATH := $(PATH):$Dprotobuf/node_modules/.bin
+	$Dprotobuf/node_modules/.bin/pbjs -t static-module -o $@ $^
 $Dprotobuf/proto.d.ts: $Dprotobuf/proto.js | $Dprotobuf/node_modules/.bin/pbts
-	pbts -o $@ $^
+	$Dprotobuf/node_modules/.bin/pbts -o $@ $^
 
 .PHONY: $Sprotobuf-all
 $Sprotobuf-all: $Sprotobuf-build
