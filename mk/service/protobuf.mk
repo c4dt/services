435c822431deb774fcfe314d5735150c3b62f04e
tharvik <tharvik@users.noreply.github.com>
2020-09-29 16:38:09 +0200

    protobuf: use npx

--- mk/service/protobuf.mk
+++ mk/service/protobuf.mk
@@ -3,18 +3,12 @@ $Sall: $Sprotobuf-all
 .PHONY: $Sprotobuf-build
 $Sprotobuf-build: $Dprotobuf/proto.json
 
-$Dprotobuf/node_modules/protobufjs/.installed:
-	cd $Dprotobuf && npm i protobufjs
-	touch $@
-$Dprotobuf/node_modules/.bin/pbjs: | $Dprotobuf/node_modules/protobufjs/.installed
-$Dprotobuf/node_modules/.bin/pbts: | $Dprotobuf/node_modules/protobufjs/.installed
-
-$Dprotobuf/proto.json: | $Dprotobuf/node_modules/.bin/pbjs
-	$Dprotobuf/node_modules/.bin/pbjs -t json -o $@ $^
-$Dprotobuf/proto.js: | $Dprotobuf/node_modules/.bin/pbjs
-	$Dprotobuf/node_modules/.bin/pbjs -t static-module -o $@ $^
-$Dprotobuf/proto.d.ts: $Dprotobuf/proto.js | $Dprotobuf/node_modules/.bin/pbts
-	$Dprotobuf/node_modules/.bin/pbts -o $@ $^
+$Dprotobuf/proto.json:
+	npx -p protobufjs pbjs -t json -o $@ $^
+$Dprotobuf/proto.js:
+	npx -p protobufjs pbjs -t static-module -o $@ $^
+$Dprotobuf/proto.d.ts: $Dprotobuf/proto.js
+	npx -p protobufjs pbts -o $@ $^
 
 .PHONY: $Sprotobuf-all
 $Sprotobuf-all: $Sprotobuf-build
