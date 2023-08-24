9cb6dc98189807485939071543ad24f0d0a7e73c
Linus Gasser <linus.gasser@epfl.ch>
2023-08-24 12:58:13 +0200

    Using hack for pbjs and pbts

--- mk/service/webapp.mk
+++ mk/service/webapp.mk
@@ -11,10 +11,10 @@ ifneq ($(wildcard $Dprotobuf),)
 # TODO for now, we can only generate for flat protobuf hierarchy
 $Dwebapp/src/lib/proto.js: private PATH := $(PATH):$Dwebapp/node_modules/protobufjs/bin
 $Dwebapp/src/lib/proto.js: $(foreach p,$($SPROTOS),$Dprotobuf/$p.proto) | $Dwebapp/node_modules
-	pbjs -t static-module -o $@ $^
+	npm i -g protobufjs@6 && pbjs -t static-module -o $@ $^
 $Dwebapp/src/lib/proto.d.ts: private PATH := $(PATH):$Dwebapp/node_modules/protobufjs/bin
 $Dwebapp/src/lib/proto.d.ts: $Dwebapp/src/lib/proto.js | $Dwebapp/node_modules
-	pbts -o $@ $<
+	npm i -g protobufjs@6 && pbts -o $@ $<
 
 .PHONY: $Swebapp-proto
 $Swebapp-proto: $Dwebapp/src/lib/proto.js $Dwebapp/src/lib/proto.d.ts
